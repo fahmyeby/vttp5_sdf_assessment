43ec004d575fba6cfbb7ec8368da7eaf56e16c40
fahmyeby <fahmyebyyussoff@yahoo.com.sg>
2024-10-30 16:21:29 +0800

    test push

---
+++ vttp_b5_assessment_template/task01/src/vttp/batch5/sdf/task01/Main.java
@@ -0,0 +1,181 @@
+package vttp.batch5.sdf.task01;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+
+	
+	static class RiderData {
+		//initialize variable
+		int day; 
+		int season; 
+		int totalRiders;
+		int weather; 
+		boolean holiday; 
+		int month; 
+
+		// constructor
+		public RiderData(int day, int season, int totalRiders, int weather, boolean holiday, int month) {
+			this.day = day;
+			this.season = season;
+			this.totalRiders = totalRiders;
+			this.weather = weather;
+			this.holiday = holiday;
+			this.month = month;
+		}
+
+		// *** check if need or not???
+		@Override
+		public String toString() {
+			return "RiderData [Day=" + getDay(day) + ", Season=" + getSeason(season) + ", Total Riders=" + totalRiders +
+					", Weather=" + getWeather(weather) + ", Holiday="
+					+ (holiday ? "was a holiday" : "was not a holiday") +
+					", Month=" + getMonth(month) + "]";
+		}
+
+		// methods to convert int to string
+		private String getDay(int day) {
+			switch (day) {
+				case 0:
+					return "Sunday";
+				case 1:
+					return "Monday";
+				case 2:
+					return "Tuesday";
+				case 3:
+					return "Wednesday";
+				case 4:
+					return "Thursday";
+				case 5:
+					return "Friday";
+				case 6:
+					return "Saturday";
+				default:
+					return "Invalid";
+			}
+		}
+
+		private String getSeason(int season) {
+			switch (season) {
+				case 1:
+					return "Spring";
+				case 2:
+					return "Summer";
+				case 3:
+					return "Fall";
+				case 4:
+					return "Winter";
+				default:
+					return "Invalid";
+			}
+		}
+
+		private String getMonth(int month) {
+			switch (month) {
+				case 1:
+					return "January";
+				case 2:
+					return "February";
+				case 3:
+					return "March";
+				case 4:
+					return "April";
+				case 5:
+					return "May";
+				case 6:
+					return "June";
+				case 7:
+					return "July";
+				case 8:
+					return "August";
+				case 9:
+					return "September";
+				case 10:
+					return "October";
+				case 11:
+					return "November";
+				case 12:
+					return "December";
+				default:
+					return "Invalid";
+			}
+		}
+
+		private String getWeather(int weather) {
+			switch (weather) {
+				case 1:
+					return "Clear, Few clouds, Partly cloudy, Partly cloudy";
+				case 2:
+					return "Mist + Cloudy, Mist + Broken clouds, Mist + Few clouds, Mist";
+				case 3:
+					return "Light Snow, Light Rain + Thunderstorm + Scattered clouds, Light Rain + Scattered clouds";
+				case 4:
+					return "Heavy Rain + Ice Pallets + Thunderstorm + Mist, Snow + Fog";
+				default:
+					return "Invalid";
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		String csvFile = "vttp_b5_assessment_template/task01/day.csv";//csv file path
+		String line; 
+		boolean header = true; 
+		List<RiderData> riderDataList = new ArrayList<>();
+
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(csvFile));
+			//skip header
+			while (header) {
+				header = false;
+			}
+			while ((line = br.readLine()) != null) {
+				String[] columns = line.split(",");
+
+				try {
+					//parse csv
+					int season = Integer.parseInt(columns[0].trim());
+					int month = Integer.parseInt(columns[1].trim());
+					int holidayValue = Integer.parseInt(columns[2].trim());
+					int day = Integer.parseInt(columns[3].trim());
+					int weather = Integer.parseInt(columns[4].trim());
+					int col8 = Integer.parseInt(columns[8].trim());
+					int col9 = Integer.parseInt(columns[9].trim());
+					int totalRiders = col8 + col9; 
+					boolean holiday = (holidayValue == 1); // convert from int to boolean
+					riderDataList.add(new RiderData(day, season, totalRiders, weather, holiday, month));
+
+				} catch (NumberFormatException e) {
+					e.printStackTrace();
+				}
+			}
+
+			// sort list form big to small
+			riderDataList.sort(new Comparator<RiderData>() {
+				@Override
+				public int compare(RiderData r1, RiderData r2) {
+					return Integer.compare(r2.totalRiders, r1.totalRiders); 
+				}
+			});
+
+			//filter top5
+			List<RiderData> top5Riders = riderDataList.size() > 5 ? riderDataList.subList(0, 5) : riderDataList;
+
+			//output
+			for (RiderData riderData : top5Riders) {
+				System.out.println("\nThe (position) recorded number of cyclists was in " + riderData.getSeason(riderData.season) + " (season), on a " + riderData.getDay(riderData.day) + " (day) in the month of " +riderData.getMonth(riderData.month) + " (month). There were a total of " + riderData.totalRiders + " (total) cylists. The weather was " + riderData.getWeather(riderData.weather) + " (weather). " + riderData.getDay(riderData.day) + " (day) " + (riderData.holiday ? "was a holiday" : "was not a holiday"));
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+
+
---
+++ vttp_b5_assessment_template/task02/src/vttp/batch5/sdf/task02/Main.java
@@ -0,0 +1,114 @@
+package vttp.batch5.sdf.task02;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+
+		if (args.length != 1) {
+			System.out.println("Add in path to .txt file when running");
+			System.out.println("Format: java -cp classes vttp.batch5.sdf.task02.Main <TTT/figure1.txt>");
+			System.out.println("Where <> is .txt file name");
+
+		}
+
+		String txtFile = args[0];
+		char[][] board = readBoard(txtFile);
+
+		System.out.println("Processing: " + txtFile);
+		System.out.println("Board:\n");
+		printBoard(board);
+
+		int[][] utilityTable = calculateUtilityTable(board);
+		System.out.println("---------------------------");
+		printUtilityTable(utilityTable);
+	}
+
+	private static char[][] readBoard(String txtFile) throws IOException {
+		char[][] board = new char[3][3];
+
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(txtFile));
+			String line;
+			int row = 0;
+			while ((line = reader.readLine()) != null && row < 3) {
+				for (int column = 0; column <3 && column < line.length(); column++) {
+					board[row][column] = line.charAt(column);
+				}
+				row++;
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		return board;
+		
+	}
+
+	private static int[][] calculateUtilityTable(char[][] board) {
+		int[][] utilityTable = new int [3][3]; //utility table 2d array
+
+		// check for row and column for best, worst and draw move
+		for (int i = 0; i<3; i++) {
+			for (int j = 0; j<3; j++){
+				if (board[i][j] == '.'){
+					if(winningMove(board, i, j, 'x')) {
+						utilityTable[i][j] = 1; // best
+					} else if (losingMove(board, i, j, 'o')){
+						utilityTable[i][j] = -1; // worst
+					} else {
+						utilityTable[i][j] = 0; //draw
+					}
+				} 
+			}
+		}
+		return utilityTable;
+	}
+
+	private static boolean winningMove(char[][] board, int i, int j , char player ) {
+		board[i][j] = player;
+		boolean win = checkIfWin(board, player);
+		board[i][j] = '.';
+		return win;
+	}
+
+	private static boolean losingMove(char[][] board, int i, int j , char cpu ) {
+		board[i][j] = cpu;
+		boolean lose = checkIfWin(board, cpu);
+		board[i][j] = '.';
+		return lose;
+	}
+
+	public static boolean checkIfWin(char[][] board, char player) {
+		for (int i = 0; i < 3; i++) {
+			if (board[i][0] == player && board[i][1] == player && board[i][2] == player) {
+				return true;
+			}
+			if (board[0][i] == player && board[1][i] == player && board[2][i] == player) {
+				return true;
+			}
+		}
+		return (board[0][0] == player && board[1][1] == player && board[2][2] == player) ||
+				(board[0][2] == player && board[1][1] == player && board[2][0] == player);
+	}
+
+	private static void printBoard(char[][] board) {
+		for (int i=0; i <3; i++) {
+			for (int j=0; j<3;j++) {
+				System.out.println(board[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void printUtilityTable(int[][] utilityTable) {
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				System.out.println("y=" + i + ", x=" + j + ",     utility=" + utilityTable[i][j]);
+			}
+		}
+	}
+}
